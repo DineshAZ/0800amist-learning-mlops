597462be7c8abf5932711b60cf8d53ac7dd08b75
azonecloud <100863730+azonecloud@users.noreply.github.com>
2024-10-08 09:24:42 +0530

    Latest Commits

---
+++ steps-to-be-followed.md
@@ -0,0 +1,5 @@
+# NumPy Installation
+$ pip install numpy
+
+# Pandas Installation
+$ pip install pandas
--- train.py
+++ train.py
@@ -1,2 +1,12 @@
+# Importing Libraries
 import numpy as np
-data = np.array['-122.23','37.88','41.0','880.0','129.0','322.0','126.0','8.3252','452600.0','NEAR BAY']
+import pandas as pd
+
+# data = np.array['-122.23','37.88','41.0','880.0','129.0','322.0','126.0','8.3252','452600.0','NEAR BAY']
+
+
+
+# If all of your columns are the same type:
+
+x = pd.read_csv('data/housing.csv', header=0).values
+print(x)
