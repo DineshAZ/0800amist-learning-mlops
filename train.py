3a1ecbfe8f5c5b2b3e79f10be8bc6a7e8ada4589
azonecloud <100863730+azonecloud@users.noreply.github.com>
2024-10-22 09:00:21 +0530

    Latest Commits

--- train.py
+++ train.py
@@ -2,8 +2,12 @@
 import pandas as pd
 import numpy as np
 
+import json
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import root_mean_squared_error
+from sklearn.metrics import r2_score
 
 # Load the Dataset
 df = pd.read_csv('data/rental_1000.csv')
@@ -19,11 +23,40 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.20,random_s
 lr = LinearRegression()
 model = lr.fit(X_train, y_train)
 
-# Predicted the values
-predicted_rental_price = model.predict(np.array([X_test[0]]))
+# Root Mean Square Error and Score Checks
+y_pred = model.predict(X_test)
+root_mean_squared_error_score = root_mean_squared_error(y_test, y_pred)
+r2_score_value = r2_score(y_test, y_pred)
+
+# User Entries Using Inputs
+#rooms = int(input("Enter the no.of rooms:"))
+#sqft = int(input("Enter the area in Sqft:"))
+#user_input = np.array([[rooms,sqft]])
+
+# Using Data as JSON Entry
+#data = '{"rooms": 2, "sqft": 1000 }'
+#user_input = json.loads(data)
+
+# Using Data as JSON File
+with open('data/inputs.json', 'r') as f:
+  user_input = json.load(f)
+
+# User Entries Using JSON
+rooms = user_input['rooms']
+sqft = user_input['sqft']
+user_input_prediction= np.array([[rooms,sqft]])
+
+print("No.of Rooms:",user_input['rooms'])
+print("Area in Sqft:",user_input['sqft'])
+
+# Model Predicition
+predicted_rental_price = model.predict(user_input_prediction)
 
 print("####################################### Prediction Started Using Model #######################################")
-print(f"Actual Rental Price with Rooms: {X_test[0][0]} and Sqft + {X_test[0][1]} is {y_test[0]}")
-print(f"Predicted Rental Price with Rooms: {X_test[0][0]} and Sqft + {X_test[0][1]} is {predicted_rental_price}")
+print(f"Predicted Rental Price with Rooms={rooms} and Sqft={sqft} is {predicted_rental_price[0]:2f}")
 print("####################################### Prediction Concluded Using Model #######################################")
+
+print("Root Mean Squared Error for Built Model",root_mean_squared_error_score)
+print("Accuary Score for Built Model",r2_score_value)
+
 
